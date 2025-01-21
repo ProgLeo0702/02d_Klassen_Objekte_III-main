ec7841fa117ce9789b6442fdf3eee2166da8866a
ProgLeo0702 <leon-ahner@gmx.de>
2025-01-21 09:30:11 +0100

    1. /getAge() / isFemale

---
+++ com/cc/java/Cat.java
@@ -0,0 +1,48 @@
+package com.cc.java;
+
+public class Cat {
+
+    private String firstName;
+    private String furColor;
+    private int age;
+    private boolean isFemale;
+
+    
+    public Cat(String firstName, String furColor, int age, boolean isFemale) {
+        this.firstName = firstName;
+        this.furColor = furColor;
+        this.age = age;
+        this.isFemale = isFemale;
+    }
+
+    
+
+    /** Getter */
+    public String getStringAttributes(String op) {
+      switch (op) {
+        case "#name":
+            return firstName;
+        case "#color":
+            return furColor;
+        default:
+            return "ERROR";
+      }
+    }
+
+    public String getAge() {
+
+        if (isFemale) {
+            return "This is an inappropriate question!";
+        } else {
+            return Integer.toString(age);
+        }
+
+
+        
+    }
+
+    
+  
+
+
+}
